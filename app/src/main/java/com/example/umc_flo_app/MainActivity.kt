2ba0c89709e602b660af93f9231947679733c252
Seannnn55 <sally99h@gmail.com>
2023-11-20 13:46:52 +0900

    rsc: 이미지 리소스 추가

--- app/src/main/java/com/example/umc_flo_app/MainActivity.kt
+++ app/src/main/java/com/example/umc_flo_app/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.umc_flo_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.umc_flo_app.ui.theme.UMC_FLO_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Android","from Emma")
                 }
             }
         }
@@ -30,17 +36,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(name: String, name2: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(8.dp)
+        )
+        Text(
+            text = "$name2",
+            modifier = modifier.align(alignment = Alignment.End)
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     UMC_FLO_AppTheme {
-        Greeting("Android")
+        Greeting("Android","from Emma")
     }
 }
